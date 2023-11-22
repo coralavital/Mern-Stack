5085b31d3b709cc597ac36d35b9a8e58aad3e703
Coral Avital <coralavital@icloud.com>
2023-11-22 13:35:22 +0200

    adding authForm component

---
+++ client/src/components/AuthForm/styles.js
@@ -0,0 +1,13 @@
+const styles = {
+    container: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'start',
+        margin: '6rem',
+    },
+    card: { width: '400px', },
+    text: { fontSize: '15px', },
+    title: { textAlign: 'center', fontSize: '25px'},
+};
+
+export default styles;
