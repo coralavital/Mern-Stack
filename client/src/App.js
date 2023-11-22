529eb520baf35516dc2cd179d492d9188d329476
Coral Avital <coralavital@icloud.com>
2023-11-22 13:42:07 +0200

    add client/package-lock

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Layout } from 'antd';
 
+import Home from './components/Home';
+import styles from './styles';
+import AppBar from './components/AppBar/AppBar';
+import AuthForm from './components/AuthForm/AuthForm';
+
+const { Footer } = Layout;
 
 const App = () => {
   return (
-    <div></div>
+    <BrowserRouter>
+      <Layout style={styles.layout}>
+        <AppBar />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/authform' element={<AuthForm  />}/>
+        </Routes>
+        <Footer style={styles.footer}>2023 Coral Avital Instaverse</Footer>
+      </Layout>
+    </BrowserRouter>
   );
 };
 
