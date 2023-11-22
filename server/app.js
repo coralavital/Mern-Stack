da90674f442dae23573cd071843f04d95f82f88d
Coral Avital <coralavital@icloud.com>
2023-11-22 13:28:40 +0200

    adding userRoutes to app.js

--- server/app.js
+++ server/app.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ limit: "32mb", extended: true }));
 app.use(cors());
 
 app.use("/stories", storyRoutes);
+app.use("/user", userRoutes);
 
 const MONGO_URI = process.env.MONGO_URI;
 const PORT = process.env.PORT || 5001;
