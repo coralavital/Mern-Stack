7f5f9fd2b1390f264584c35d5bd33a22925dc1a7
Coral Avital <coralavital@icloud.com>
2023-12-17 14:45:39 +0200

    deploy backend side with render

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'https://instaverse-api-12e5dbb92606.herokuapp.com/' });
+const api = axios.create({ baseURL: 'https://likeu-api.onrender.com/' });
 
 api.interceptors.request.use((req) => {
 
